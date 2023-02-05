6d3fc20be98c3c7437f9a898737f19f5412c7076
rafa6galvao <rafagalvaonull@gmail.com>
2023-02-05 16:17:57 -0300

    Conversão Moeda V0

--- Mundo 01/Desafio 10 -Dolares - V0.py
+++ Mundo 01/Desafio 10 -Dolares - V0.py
@@ -7,3 +7,4 @@ cor2 = '\033[1;30;41m'
 din = float(input('Digite quanto em reais você tem na carteira{}: R$ '.format(cr)))
 dol = din / 5.50
 print('Você pode {}comprar{} com {:.2f}R$ a quantia de {}{:.2f}${}{}'.format(cor1, cor0, din, cor2, dol, cd, cor0))
+
