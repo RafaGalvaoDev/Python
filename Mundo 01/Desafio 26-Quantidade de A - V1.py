dbcee8a38aa6eb41465ce21da83af323d903b362
rafa6galvao <rafagalvaonull@gmail.com>
2023-02-05 16:58:11 -0300

    Quantidade de A V1

--- Mundo 01/Desafio 26-Quantidade de A - V1.py
+++ Mundo 01/Desafio 26-Quantidade de A - V1.py
@@ -6,3 +6,4 @@ qa = fm.count('a')
 print('A letra A aparece primeiro na possição {}'.format(pa+1))
 print('A letra A aparece {} vezes '.format(qa))
 print('A letra A aparece por último na possição {}'.format(ua+1))
+
