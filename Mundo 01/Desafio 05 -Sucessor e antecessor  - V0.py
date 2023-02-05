6a6dd9312b05cec7b16cb6b44c50f1979584e91b
rafa6galvao <rafagalvaonull@gmail.com>
2023-02-05 15:26:03 -0300

    Sucessor e Antecessor V0

--- Mundo 01/Desafio 05 -Sucessor e antecessor  - V0.py
+++ Mundo 01/Desafio 05 -Sucessor e antecessor  - V0.py
@@ -8,3 +8,4 @@ verde = '\033[4;32m'
 vermelho = '\033[4;31m'
 semCor = '\033[m'
 print('O número digitado foi {} o seu {}antecessor{} {} é {} e seu {}sucessor{} {} é {}'.format(numero, verde, semCor, vant, ant, vermelho, semCor, vsuc, suc))
+
