69c8c03193bb2b3c5c33f27c8eee8bffc0b869ad
rafa6galvao <rafagalvaonull@gmail.com>
2023-02-05 16:28:31 -0300

    Desconto de 5% V0

--- Mundo 01/Desafio 12-Desconto 5% - V0.py
+++ Mundo 01/Desafio 12-Desconto 5% - V0.py
@@ -9,3 +9,4 @@ cor1 = '\033[4;30;42m'
 cor2 = '\033[4;30;41m'
 print('O {}valor do produto era {} R${} e com 5% de desconto {}passa a ser {}R${}{}'.format(cor2, preco, cor0, cor1, novoPreco, cor0, din))
 print('Um {}desconto de {} R${}{}'.format(cor1, desconto, cor0, din1))
+
