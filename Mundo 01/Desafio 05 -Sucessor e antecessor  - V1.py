44de740e77c29cae7d57b583ae584726f95086e8
rafa6galvao <rafagalvaonull@gmail.com>
2023-02-05 15:26:49 -0300

    Sucessor e Antecessor V1

--- Mundo 01/Desafio 05 -Sucessor e antecessor  - V1.py
+++ Mundo 01/Desafio 05 -Sucessor e antecessor  - V1.py
@@ -2,3 +2,4 @@ numero = int(input('Digite um número inteiro qualquer: '))
 ant = numero - 1
 suc = numero + 1
 print('O número digitado {} tem seu sucessor {}, e o antecessor {}'.format(numero, suc, ant))
+
