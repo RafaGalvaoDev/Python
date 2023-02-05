4bf0f5e1a448417f58d3b14d56c39bbbf75aa8e7
rafa6galvao <rafagalvaonull@gmail.com>
2023-02-05 16:29:30 -0300

    Desconto de 5% V1

--- Mundo 01/Desafio 12-Desconto 5% - V1.py
+++ Mundo 01/Desafio 12-Desconto 5% - V1.py
@@ -4,3 +4,4 @@ novoPreco = preco - desconto
 print('O valor do produto é {:.2f}R$'.format(preco))
 print('O valor com 5% de desconto é {:.2f}R$'.format(novoPreco))
 print('Um desconto de {:.2f}R$'.format(desconto))
+
