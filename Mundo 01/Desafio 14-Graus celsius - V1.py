c8baff97d3d7e3f184e721d2951ba0e9f51a64d5
rafa6galvao <rafagalvaonull@gmail.com>
2023-02-05 16:34:17 -0300

    Graus Celsius V1

--- Mundo 01/Desafio 14-Graus celsius - V1.py
+++ Mundo 01/Desafio 14-Graus celsius - V1.py
@@ -1,4 +1,3 @@
 graus = float(input('Digite o valor da temperatura em graus: '))
 f = (graus * 1.8) + 32
 print('A temperatura de {} graus celsius é igual a {:.2f} graus fahrenheit'.format(graus, f))
-
