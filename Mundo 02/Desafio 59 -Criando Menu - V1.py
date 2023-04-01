690011ee40207c4a3f02b5de81305234ac1c3730
rafa6galvao <rafagalvaonull@gmail.com>
2023-04-01 13:01:46 -0300

    Criando Menu V1

---
+++ Mundo 02/Desafio 59 -Criando Menu - V1.py
@@ -0,0 +1,26 @@
+n1 = int(input('Digite um valor: '))
+n2 = int(input('Digite outro valor: '))
+cond = 0
+while cond != 5:
+    print('''Digite [1] para Somar\nDigite [2] para Múltiplicar\nDigite [3] para Maior\nDigite [4] para Novos Número\nDigite [5] para Sair''')
+    cond = int(input('Digite aqui:'))
+    if cond == 1:
+        soma = n1 + n2
+        print('A soma de {} + {} é igual a {}'.format(n1, n2, soma))
+    elif cond == 2:
+        mult = n1 * n2
+        print('A múltiplicação de {} x {} é igual a {}'.format(n1, n2, mult))
+    elif cond == 3:
+        if n1 > n2:
+            print('{} é o maior'.format(n1))
+        else:
+            print('{} é o maior'.format(n2))
+    elif cond == 4:
+        print('Digite os Novos Números')
+        n1 = int(input('Digite o número: '))
+        n2 = int(input('Digite o número: '))
+    elif cond == 5:
+        print('Desligando...')
+    else:
+        print('Número invalido, tente novamente')
+print('Até a Proxima')
