681e55476a100bc8078a89ff5cf9757964e631ba
rafa6galvao <rafagalvaonull@gmail.com>
2023-04-06 08:37:30 -0300

    Termos

---
+++ Mundo 02/Desafio 61 -10 Termos - V1.py
@@ -0,0 +1,8 @@
+termo = int(input('Digite o termo: '))
+razao = int(input('Digite a razão: '))
+ptermo = termo
+cont = 1
+while cont <= 10:
+    print('{}'.format(termo))
+    termo = termo + razao
+    cont = cont + 1
