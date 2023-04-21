3d5bac9cfc4222d73a1b1bd90a94a4a17199e3c3
rafa6galvao <rafagalvaonull@gmail.com>
2023-04-21 10:23:01 -0300

    Compras V2

---
+++ Mundo 02/Desafio 70- Campras - V2.py
@@ -0,0 +1,20 @@
+soma = mil = menor = c = 0
+barat = ''
+while True:
+    preco = float(input('Digite o valor do produto em R$: '))
+    nomeProduto = str(input('Digite o nome do produto: ')).strip().upper()
+    soma += preco
+    c += 1
+    if preco >= 1000:
+        mil += 1
+    if c == 1 or preco < menor:
+        menor = preco
+        barat = nomeProduto
+    continuar = ' '
+    while continuar not in 'SN':
+        continuar = str(input('Dejesa continuar? S/N: ')).strip().upper()[0]
+    if continuar == 'N':
+        break
+print(f'O valor da sua compra foi {soma:.2f}')
+print(f'{mil} proutos custaram 1000 R$ ou mais')
+print(f'O produto mais barato foi {barat} custando {menor:.2f} R$')
