916ef621a1b3f1863b9238746168344f7e6b2e13
rafa6galvao <rafagalvaonull@gmail.com>
2023-04-24 13:17:53 -0300

    Times

---
+++ Mundo 03/Desafio 73 - Times - V1.py
@@ -0,0 +1,11 @@
+while True:
+    times = ('Palmeiras', 'Internacional', 'Fluminense', 'Flamengo', 'Corinthians', 'Atlético-MG', 'Athletico', 'América-MG', 'Botafogo', 'São Paulo', 'Fortaleza', 'Santos', 'Chapecoense', 'Goiás', 'Coritiba', 'Cuiabá', 'Cruzeiro', 'Grêmio', 'Bahia', 'Vasco')
+    print("/-_-/" * 12)
+    print(f'Os 5 primeiros times são {times[0:5]}')
+    print('/-_-/' * 12)
+    print(f'Os 4 últimos times são {times[-4:]}')
+    print('/-_-/' * 12)
+    print(f'Os times em ordem alfabetica é {sorted(times)}')
+    print('/-_-/' * 12)
+    print(f'O time do Chapecoense esta na possição {len("Chapecoense")}')
+    break
