c6ce2679f41a96d049f8abca567a93b0224074ee
rafa6galvao <rafagalvaonull@gmail.com>
2023-06-02 10:39:18 -0300

    Função Ficha Jogador V1

---
+++ Mundo 03/Desafio 103 -Função Ficha Jogaodor - V1.py
@@ -0,0 +1,19 @@
+def ficha(nome='Desconhecido', gols=0):
+    if gols == 0:
+        print(f'O Jogador {nome} fez {gols} Gols no Campeonato')
+    elif gols > 1:
+        print(f'O Jogador {nome} fez {gols} Gols no Campeonato')
+    else:
+        print(f'O Jogador {nome} fez {gols} Gol no Campeonato')
+
+
+nome = str(input('Digite o Nome do Jogador: '))
+gols = str(input(f'Digite Quantos gols o Jogador {nome} Marcou: '))
+if gols.isnumeric():
+    gols = int(gols)
+else:
+    gols = 0
+if nome.strip() == '':
+    ficha(gols=gols)
+else:
+    ficha(nome, gols)
