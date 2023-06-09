bd7b4db76f84eca40c3652ab02a6bc5e477ccfe7
rafa6galvao <rafagalvaonull@gmail.com>
2023-06-09 12:54:45 -0300

    Defs Desafio 107

---
+++ Mundo 03/Desafios 107 - Minhas_Defs/Moedas.py
@@ -0,0 +1,20 @@
+def aumentar(n, taxa):
+    n = n + (n * taxa / 100)
+    return n
+
+
+def diminuir(n, taxa):
+    n = n - (n * taxa / 100)
+    return n
+
+
+def dobro(n):
+    n *= 2
+    return n
+
+
+def metade(n):
+    n = n / 2
+    return n
+
+
