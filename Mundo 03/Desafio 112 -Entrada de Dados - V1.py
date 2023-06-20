c97cd1c7b87e0033fd8550c7a9cb54ae28b36eb6
rafa6galvao <rafagalvaonull@gmail.com>
2023-06-20 09:22:09 -0300

    Entrada de Dados

---
+++ Mundo 03/Desafio 112 -Entrada de Dados - V1.py
@@ -0,0 +1,4 @@
+from Minhas_Defs.utilidade_curso_em_video import Moeda
+from Minhas_Defs.utilidade_curso_em_video import Dados
+numero = Dados.leiadinheiro('Digite Um Valor: ')
+Moeda.resumo(numero, 23, 15)
